156cb045a96b7d020545fa7d893d5d46a436b100
ChilimanTube <chilimantube@gmail.com>
2024-03-21 22:30:15 +0100

    feat: mantine config -> causing hook error

--- apps/web/emr/src/pages/_app.tsx
+++ apps/web/emr/src/pages/_app.tsx
@@ -1,6 +1,22 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import dynamic from 'next/dynamic';
+import { createTheme, ColorSchemeScript } from '@mantine/core';
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
+
+const MantineProvider = dynamic(
+  () => import('@mantine/core').then((mod) => mod.MantineProvider),
+  { ssr: false }  // This line will disable server-side rendering for MantineProvider
+);
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <MantineProvider theme={theme}>
+      <ColorSchemeScript defaultColorScheme="auto" />
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
