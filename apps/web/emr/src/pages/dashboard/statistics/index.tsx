5eb3e80e6baf579ac346ccb7ca3fef162da5c887
Vojtěch Král <kralvojtech@email.cz>
2024-03-25 18:39:15 +0100

    feat: stats page initialiation

--- apps/web/emr/src/pages/dashboard/statistics/index.tsx
+++ apps/web/emr/src/pages/dashboard/statistics/index.tsx
@@ -3,8 +3,9 @@ import { Group, Paper, SimpleGrid, Text } from '@mantine/core';
 import {
     IconUserPlus,
     IconDiscount2,
-    IconReceipt2,
-    IconCoin,
+    IconMedicalCrossFilled,
+    IconMedicalCross,
+    IconCheck,
     IconArrowUpRight,
     IconArrowDownRight,
   } from '@tabler/icons-react';
@@ -14,15 +15,15 @@ import {
   const icons = {
     user: IconUserPlus,
     discount: IconDiscount2,
-    receipt: IconReceipt2,
-    coin: IconCoin,
+    medic: IconMedicalCross,
+    success: IconCheck,
   };
 
   const data = [
-    { title: 'Revenue', icon: 'receipt', value: '13,456', diff: 34 },
-    { title: 'Profit', icon: 'coin', value: '4,145', diff: -13 },
-    { title: 'Coupons usage', icon: 'discount', value: '745', diff: 18 },
-    { title: 'New customers', icon: 'user', value: '188', diff: -30 },
+    { title: 'Mission Count', icon: 'medic', value: '100', diff: 34 },
+    { title: 'Successes', icon: 'success', value: '78', diff: 2 },
+    { title: 'Fails / Aborts', icon: 'discount', value: '22', diff: -51 },
+    { title: 'Success Rate', icon: 'success', value: '78', diff: +3 },
   ] as const;
 
 
@@ -32,24 +33,24 @@ import {
       const DiffIcon = stat.diff > 0 ? IconArrowUpRight : IconArrowDownRight;
   
       return (
-        <Paper withBorder p="md" radius="md" key={stat.title}>
+        <Paper withBorder p="md" radius="md" key={stat.title} className={classes.paper}>
           <Group justify="space-between">
-            <Text size="xs" c="dimmed" className={classes.title}>
-              {stat.title}
-            </Text>
-            <Icon className={classes.icon} size="1.4rem" stroke={1.5} />
+        <Text size="xs" c="dimmed" className={classes.title}>
+          {stat.title}
+        </Text>
+        <Icon className={classes.icon} size="1.4rem" stroke={1.5} />
           </Group>
-  
+
           <Group align="flex-end" gap="xs" mt={25}>
-            <Text className={classes.value}>{stat.value}</Text>
-            <Text c={stat.diff > 0 ? 'teal' : 'red'} fz="sm" fw={500} className={classes.diff}>
-              <span>{stat.diff}%</span>
-              <DiffIcon size="1rem" stroke={1.5} />
-            </Text>
+        <Text className={classes.value}>{stat.value}</Text>
+        <Text c={stat.title === 'Fails / Aborts' ? (stat.diff > 0 ? 'red' : 'teal') : (stat.diff > 0 ? 'teal' : 'red')} fz="sm" fw={500} className={classes.diff}>
+          <span>{stat.diff}%</span>
+          <DiffIcon size="1rem" stroke={1.5} />
+        </Text>
           </Group>
-  
+
           <Text fz="xs" c="dimmed" mt={7}>
-            Compared to previous month
+        Compared to previous week
           </Text>
         </Paper>
       );
