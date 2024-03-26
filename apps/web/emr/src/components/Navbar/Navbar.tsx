c5a6deaeb3382f90bf61027d1cc1815ab24e4bcb
ChilimanTube <chilimantube@gmail.com>
2024-03-26 22:14:51 +0100

    feat: disabled currently unavailable buttons in Navbar

--- apps/web/emr/src/components/Navbar/Navbar.tsx
+++ apps/web/emr/src/components/Navbar/Navbar.tsx
@@ -20,17 +20,17 @@ import {
 import classes from './Navbar.module.css';
 
 const data = [
-  { link: '/dashboard', label: 'Dashboard', icon: IconCategory },
-  { link: '/dashboard/dispatch', label: 'Dispatch', icon: IconHeartRateMonitor },
-  { link: '/dashboard/teams', label: 'Teams', icon: IconUsers },
-  { link: '', label: 'Emergencies', icon: IconHeartbeat },
-  { link: '/dashboard/statistics', label: 'Statistics', icon: IconChartPie },
-  { link: '', label: 'Chat', icon: IconMessageCircle },
-  { link: '', label: 'Map', icon: IconMap },
-  { link: '', label: 'Guides', icon: IconBook },
-  { link: 'https://www.medrunner.space/emergency-response-guide', label: 'ERG', icon: IconFileDescription},
-  { link: '', label: 'Medals', icon: IconBadges },
-  { link: '', label: 'Settings', icon: IconSettings },
+  { link: '/dashboard', label: 'Dashboard', icon: IconCategory, disabled: false},
+  { link: '/dashboard/dispatch', label: 'Dispatch', icon: IconHeartRateMonitor, disabled: false},
+  { link: '/dashboard/teams', label: 'Teams', icon: IconUsers, disabled: false },
+  { link: '', label: 'Emergencies', icon: IconHeartbeat, disabled: true},
+  { link: '/dashboard/statistics', label: 'Statistics', icon: IconChartPie, disabled: false },
+  { link: '', label: 'Chat', icon: IconMessageCircle, disabled: true },
+  { link: '', label: 'Map', icon: IconMap, disabled: true },
+  { link: '', label: 'Guides', icon: IconBook, disabled: true },
+  { link: 'https://www.medrunner.space/emergency-response-guide', label: 'ERG', icon: IconFileDescription, disabled: false},
+  { link: '', label: 'Medals', icon: IconBadges, disabled: true },
+  { link: '', label: 'Settings', icon: IconSettings, disabled: true },
 ];
 
 export function Navbar() {
@@ -38,12 +38,11 @@ export function Navbar() {
 
   const links = data.map((item) => (
     <Link
-      className={classes.link}
+      className={`${classes.link} ${item.disabled ? classes.disabledLink : ''}`}
       data-active={item.label === active || undefined}
       href={item.link}
       key={item.label}
       onClick={(event) => {
-        setActive(item.label);
       }}
       target={/^https:\/\//.test(item.link) ? '_blank' : undefined}
       rel={/^https:\/\//.test(item.link) ? 'noopener noreferrer' : undefined}
@@ -52,7 +51,6 @@ export function Navbar() {
       <span>{item.label}</span>
     </Link>
   ));
-
   return (
     <nav className={classes.navbar}>
       <div className={classes.navbarMain}>
@@ -64,11 +62,11 @@ export function Navbar() {
       </div>
 
       <div className={classes.footer}>
-        <Link href="/" className={classes.link} onClick={(event) => event.preventDefault()}>
+        <Link href="/" className={`${classes.link} ${classes.disabledLink}`} onClick={(event) => event.preventDefault()}>
           <IconUser className={classes.linkIcon} stroke={1.5} />
           <span>Username</span>
         </Link>
-        <Link href="/" className={classes.link} onClick={(event) => event.preventDefault()}>
+        <Link href="/" className={`${classes.link} ${classes.disabledLink}`} onClick={(event) => event.preventDefault()}>
           <IconSwitchHorizontal className={classes.linkIcon} stroke={1.5} />
           <span>Change account</span>
         </Link>
