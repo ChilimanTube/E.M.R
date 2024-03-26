38659ec94340ea0a48d15fcfd429cef9a7dd0e06
ChilimanTube <chilimantube@gmail.com>
2024-03-26 20:08:12 +0100

    feat: old dispatch page -> teams page

---
+++ apps/web/emr/src/pages/dashboard/teams/index.tsx
@@ -0,0 +1,21 @@
+import classes from './Teams.module.css';
+import Layout from "@/layout/dashboard-layout";
+import { ArticleCard } from '@/components/DndCard/DndCard';
+import { DndList } from '@/components/DndBoard/DndBoard';
+
+export default function Dispatch(){
+    return (
+        <Layout>
+            <div className={classes.mainContainer}>
+                <h1>Teams</h1>
+                <div className={classes.container}>
+                    <div className={classes.cardSpace}>
+                        <ArticleCard />
+                        <ArticleCard />
+                        <ArticleCard />
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    );
+}
