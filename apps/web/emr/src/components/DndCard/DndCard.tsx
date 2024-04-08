c2c771d8a792058a11354c5ff51d21cdb0a4eb9c
ChilimanTube <chilimantube@gmail.com>
2024-04-08 22:32:56 +0200

    wip: card, dashboard

--- apps/web/emr/src/components/DndCard/DndCard.tsx
+++ apps/web/emr/src/components/DndCard/DndCard.tsx
@@ -89,3 +89,108 @@ export function ArticleCard() {
     </Card>
   );
 }
+
+
+
+/*
+import { IconPencil, IconPlus, IconTrash } from '@tabler/icons-react';
+import {
+  Card,
+  Image,
+  Text,
+  ActionIcon,
+  Badge,
+  Group,
+  Center,
+  Avatar,
+  useMantineTheme,
+  rem,
+  Code,
+} from '@mantine/core';
+import classes from './DndCard.module.css';
+
+const data = [
+  {
+    teamName: 'Team Alpha',
+    leader: 'Chiliman',
+    members: ['John Doe', 'Jane Doe', 'Alice Doe'],
+    status: 'Standby',
+  },
+  {
+    teamName: 'Team Bravo',
+    leader: 'Poptic',
+    members: ['John Doe', 'Jane Doe', 'Alice Doe'],
+    status: 'Standby',
+  },
+  {
+    teamName: 'Team Charlie',
+    leader: 'Milo Wilo',
+    members: ['John Doe', 'Jane Doe', 'Alice Doe'],
+    status: 'Standby',
+  },
+];
+
+
+export function ArticleCard() {
+  const linkProps = { href: '#' };
+  const theme = useMantineTheme();
+
+  const teams = data.map((team) => {
+    return (
+      <div>
+        <Card.Section>
+          <a {...linkProps}>
+            <Image src="https://i.imgur.com/9aWAt37.png" height={180} />
+          </a>
+        </Card.Section>
+
+        <Badge className={classes.rating} variant="gradient" gradient={{ from: 'yellow', to: 'red' }}>
+          Standby
+        </Badge>
+
+        <Text className={classes.title} fw={500} component="a" {...linkProps}>
+          Team Alpha
+        </Text>
+
+        <Text fz="sm" c="dimmed" lineClamp={4}>
+          Team Members, alert name and location will be shown here.
+        </Text>
+
+        <Group justify="space-between" className={classes.footer}>
+          <Center>
+            <Avatar
+              src="https://i1.sndcdn.com/artworks-PJIRtyJOeL88vnd2-3hRzbg-t500x500.jpg"
+              size={24}
+              radius="xl"
+              mr="xs"
+            />
+            <Text fz="sm" inline>
+              Demo Dispatcher
+            </Text>
+          </Center>
+
+          <Group gap={8} mr={0}>
+            <ActionIcon className={classes.action}>
+              <IconPencil
+                style={{ width: rem(16), height: rem(16) }}
+                color={theme.colors.blue[7]}
+              />
+            </ActionIcon>
+            <ActionIcon className={classes.action}>
+              <IconTrash style={{ width: rem(16), height: rem(16) }} color={theme.colors.red[6]} />
+            </ActionIcon>
+          </Group>
+        </Group>
+      </div>
+    );
+  });
+
+  return (
+    <Card radius="md" className={classes.card} shadow="0 2px 10px rgba(0, 0, 0, 0.3)">
+      {teams}
+    </Card>
+  );
+}
+
+
+*/
--- apps/web/emr/src/pages/dashboard/index.tsx
+++ apps/web/emr/src/pages/dashboard/index.tsx
@@ -1,12 +1,66 @@
 import Link from "next/link";
-import { Button } from "@mantine/core";
+import { Group, Paper, SimpleGrid, Text, Button } from '@mantine/core';
 import { Navbar } from "@/components/Navbar/Navbar";
 import Layout from "@/layout/dashboard-layout";
+import classes from './Dashboard.module.css';
+import {
+    IconCircleCheck,
+    IconMedicalCross,
+    IconUsers,
+    IconUrgent,
+  } from '@tabler/icons-react';
+
+const icons = {
+    team: IconUsers,
+    ready: IconCircleCheck,
+    medic: IconMedicalCross,
+    alert: IconUrgent,
+  };
+
+const data = [
+    { title: 'Online Teams', icon: 'team', value: '3'},
+    { title: 'Standby Teams', icon: 'ready', value: '2'},
+    { title: 'Dispatched Teams', icon: 'medic', value: '1'},
+    { title: 'Alerts today', icon: 'alert', value: '12'},
+  ] as const;
+
+export function StatsGrid() {
+    const stats = data.map((stat) => {
+      const Icon = icons[stat.icon];
+  
+      return (
+        <div className="mainContainer" key={stat.title} >
+          <Paper withBorder p="md" radius="md" key={stat.title} className={classes.paper}>
+            <Group justify="space-between">
+          <Text size="md" c="dimmed" className={classes.title}>
+            {stat.title}
+          </Text>
+          <Icon className={classes.icon} size="1.4rem" stroke={1.5} />
+            </Group>
+            <Group align="flex-end" gap="xs" mt={25}>
+          <Text className={classes.value}>{stat.value}</Text>
+            </Group>
+          </Paper>
+        </div>
+      );
+    });
+    return (
+      <div className={classes.root}>
+        <SimpleGrid cols={{ base: 1, xs: 2, md: 4 }}>{stats}</SimpleGrid>
+      </div>
+    );
+  }
+
 
 export default function Dashboard() {
     return (
-        	<Layout>
+        <Layout>
+            <div className={classes.mainContainer}>
                 <h1>Dashboard</h1>
-            </Layout>
+                <div className={classes.container}>
+                    <StatsGrid />
+                </div>
+            </div>
+        </Layout>
     );
 }
