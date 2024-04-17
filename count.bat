d02361bd598c586072c602f06334f321d444b775
ChilimanTube <chilimantube@gmail.com>
2024-04-17 10:28:01 +0200

    fix: cloc

--- count.bat
+++ count.bat
@@ -1,4 +1,10 @@
-cd ../..;
-cd C:\Users\Vojtěch\Desktop\Own Projects\E.M.R\E.M.R\apps\web\emr;
-&cloc src;
-||print "Script failed"
+cd C:\Users\Vojtěch\Desktop\Own Projects\E.M.R\E.M.R\apps\web\emr
+
+REM Run cloc and capture the exit code
+FOR /F "tokens=*" %i IN ('cloc src 2^&1') DO (
+  IF %i EQUALS 1 (
+    ECHO An error occurred while running cloc. (code: %i)
+  ) ELSE (
+    ECHO cloc execution successful. Output: %i
+  )
+)
