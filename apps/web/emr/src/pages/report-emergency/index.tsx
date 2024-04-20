ea62bf0a5413d0eea97af1e5d74990edf4821902
ChilimanTube <chilimantube@gmail.com>
2024-04-20 23:05:44 +0200

    sos

--- apps/web/emr/src/pages/report-emergency/index.tsx
+++ apps/web/emr/src/pages/report-emergency/index.tsx
@@ -4,8 +4,8 @@ import { TeamCard } from "@/components/TeamCard/TeamCard";
 import { Stepper, Select, Button, Input } from '@mantine/core';
 
 const locations = [
-    { group: 'Stanton', items: ['Hurston', 'Crusader', 'MicroTech', 'ArcCorp']},
-    { group: 'Pyro', items: ['Pyro I','Pyro II','Pyro IV','Pyro V','Pyro VI']}
+    { group: 'Stanton', items: ['Hurston', 'Crusader', 'MicroTech', 'ArcCorp'] },
+    { group: 'Pyro', items: ['Pyro I', 'Pyro II', 'Pyro IV', 'Pyro V', 'Pyro VI'] }
 ];
 
 
@@ -22,21 +22,24 @@ export default function ReportEmergency() {
             <div className={classes.container}>
                 {!showForm && (
                     <Button
-                        onClick={() => setShowForm(true)}
-                        className={classes.button}
+                        onClick={() => {
+                            setShowForm(true);
+                            setActive(0);
+                        }}
+                        className={classes.herobutton}
                         color="red"
                         size="lg"
                         variant="gradient"
-                        gradient={{ from: 'red', to:'darkRed', deg: 90 }}                        
-                     >
+                        gradient={{ from: 'red', to: 'darkRed', deg: 90 }}
+                    >
                         SOS
-                     </Button>
+                    </Button>
                 )}
 
                 <div className={`${classes.formContainer} ${showForm ? classes.fadeIn : classes.fadeOut}`}>
                     {showForm && (
 
-                        <Stepper active={active} onStepClick={setActive} color="red" classNames={{separator: classes.separator}}>
+                        <Stepper active={active} onStepClick={setActive} color="red" classNames={{ separator: classes.separator }}>
                             <Stepper.Step label="Location" description="Select the location">
                                 <Select
                                     label="Select the location of the emergency"
@@ -46,25 +49,75 @@ export default function ReportEmergency() {
                                     required
                                     comboboxProps={{
                                         shadow: 'md',
-                                        transitionProps: { transition: 'pop', duration: 200 }
+                                        transitionProps: { transition: 'pop', duration: 200 },
                                     }}
                                     className={classes.select}
+                                    labelProps={{ className: classes.label }}
                                 />
+                                <div className={classes.buttonContainer}>
+                                    <Button onClick={prevStep} color="red" variant="light" className={classes.button}>
+                                        Previous
+                                    </Button>
+                                    <Button onClick={nextStep} color="red" variant="light" className={classes.button}>
+                                        Next
+                                    </Button>
+                                </div>
+
                             </Stepper.Step>
                             <Stepper.Step label="Clients" description="Number of clients">
                                 <Input />
+                                <div className={classes.buttonContainer}>
+                                    <Button onClick={prevStep} color="red" variant="light" className={classes.button}>
+                                        Previous
+                                    </Button>
+                                    <Button onClick={nextStep} color="red" variant="light" className={classes.button}>
+                                        Next
+                                    </Button>
+                                </div>
                             </Stepper.Step>
                             <Stepper.Step label="Injuries" description="Highest Tier injury">
                                 Step 3 content: Get full access
+                                <div className={classes.buttonContainer}>
+                                    <Button onClick={prevStep} color="red" variant="light" className={classes.button}>
+                                        Previous
+                                    </Button>
+                                    <Button onClick={nextStep} color="red" variant="light" className={classes.button}>
+                                        Next
+                                    </Button>
+                                </div>
                             </Stepper.Step>
                             <Stepper.Step label="CrimeStat" description="Highest CrimeStat Level">
                                 Step 3 content: Get full access
+                                <div className={classes.buttonContainer}>
+                                    <Button onClick={prevStep} color="red" variant="light" className={classes.button}>
+                                        Previous
+                                    </Button>
+                                    <Button onClick={nextStep} color="red" variant="light" className={classes.button}>
+                                        Next
+                                    </Button>
+                                </div>
                             </Stepper.Step>
                             <Stepper.Step label="Time Left" description="Time left before death">
                                 Step 3 content: Get full access
+                                <div className={classes.buttonContainer}>
+                                    <Button onClick={prevStep} color="red" variant="light" className={classes.button}>
+                                        Previous
+                                    </Button>
+                                    <Button onClick={nextStep} color="red" variant="light" className={classes.button}>
+                                        Next
+                                    </Button>
+                                </div>
                             </Stepper.Step>
                             <Stepper.Completed>
                                 Completed, click back button to get to previous step
+                                <div className={classes.buttonContainer}>
+                                    <Button onClick={prevStep} color="red" variant="light" className={classes.button}>
+                                        Previous
+                                    </Button>
+                                    <Button onClick={() => console.log("Submit")} color="red" variant="light" className={classes.button}>
+                                        Submit
+                                    </Button>
+                                </div>
                             </Stepper.Completed>
                         </Stepper>
                     )}
